a601fda60ca252a3690535280fc8d0c549d4dcf7
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-24 17:13:30 -0300

    user page added

---
+++ src/pages/User/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+export const Header = styled.View`
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+export const Avatar = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background: #eee;
+`;
+export const Name = styled.Text`
+  font-size: 20px;
+  color: #333;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+export const Bio = styled.Text`
+  font-size: 15px;
+  color: #999;
+  text-align: center;
+`;
