6b8f99d7d9c38d2acd418ad35ae398f1abb11ea1
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-25 10:11:24 -0300

    refresh list & infinite scroll

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -29,22 +29,47 @@ export default class User extends Component {
 
   state = {
     stars: [],
-    loading: false,
+    loading: true,
+    page: 1,
+    refreshing: false,
   };
 
   async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
     const { navigation } = this.props;
-    this.setState({ loading: true });
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: {
+        page,
+      },
+    });
 
-    this.setState({ stars: response.data, loading: false });
-  }
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      loading: false,
+      page,
+      refreshing: false,
+    });
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+    const NewPage = page + 1;
+    this.load(NewPage);
+  };
+
+  refreshList = () => {
+    this.setState({ page: 1, stars: [], refreshing: true }, this.load);
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars, loading } = this.state;
+    const { stars, loading, refreshing } = this.state;
     const user = navigation.getParam('user');
 
     return (
@@ -58,6 +83,10 @@ export default class User extends Component {
           <ActivityIndicator color="#7159c1" />
         ) : (
           <Stars
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             data={stars}
             keyExtractor={star => String(star.id)}
             renderItem={({ item }) => (
